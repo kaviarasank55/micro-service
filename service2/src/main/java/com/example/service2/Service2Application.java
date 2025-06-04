cdf79cc609d4f307361b88cb809541c81d17864c
kaviarasank55 <kaviarasan16055@mech.ssn.edu.in>
2025-06-04 19:34:55 +0530

    service discovery

--- service2/src/main/java/com/example/service2/Service2Application.java
+++ service2/src/main/java/com/example/service2/Service2Application.java
@@ -2,6 +2,8 @@ package com.example.service2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Service2Application {
@@ -9,5 +11,9 @@ public class Service2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Service2Application.class, args);
 	}
-
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
--- service2/src/main/java/com/example/service2/controller/Controller.java
+++ service2/src/main/java/com/example/service2/controller/Controller.java
@@ -1,15 +1,31 @@
 package com.example.service2.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
+	
+	@Autowired
+	private DiscoveryClient client;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Service 2!";
+    	List<ServiceInstance> lstofInstance=client.getInstances("service-1");
+    	String uri=lstofInstance.get(0).getUri().toString();
+    	Object res= restTemplate.getForObject(uri+"/api/hello", Object.class);
+        return "Hello from Service 2!   ->>" + res;
     }
 }
 
